5a98dd685633392fbf85c739cd7a13ccd1317452
Ruan <ruan.klein@gmail.com>
2026-03-19 22:36:41 -0300

    chore: update Synapseq to version 4.0.0 and correct repository links

--- Formula/synapseq.rb
+++ Formula/synapseq.rb
@@ -1,25 +1,25 @@
 class Synapseq < Formula
-  desc "Synapse-Sequenced Brainwave Generator"
-  homepage "https://github.com/ruanklein/synapseq"
-  version "3.5.1"
+  desc "Text-Driven Audio Sequencer for Brainwave Entrainment"
+  homepage "https://github.com/synapseq-foundation/synapseq"
+  version "4.0.0"
   license "GPL-2.0-only"
 
-  base_url = "https://github.com/ruanklein/synapseq/releases/download/v#{version}"
+  base_url = "https://github.com/synapseq-foundation/synapseq/releases/download/v#{version}"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "#{base_url}/synapseq-v#{version}-macos-arm64.tar.gz"
-      sha256 "5a76a821596efe5afcbcaa9cbe7b692802cd1c9b6daa24578c17cfed9fa10603"
+      sha256 "ce27b95993025ddfb8d6cf070daf3c0be662f49b5428be6f5e98e107547cd64d"
     else
       odie "SynapSeq is only available for macOS ARM64 (Apple Silicon)."
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "#{base_url}/synapseq-v#{version}-linux-arm64.tar.gz"
-      sha256 "fea99811089dd3fb9c91f2998202d852f515466d5bdaa942a8a64dd12e0bf492"
+      sha256 "445ef7debf53de2c8e6e65804d650fd24b6da79428839d30d46e6aee80031d19"
     elsif Hardware::CPU.intel?
       url "#{base_url}/synapseq-v#{version}-linux-amd64.tar.gz"
-      sha256 "d7b0d0432809f259659eb4224e9b4bee2ce065a69004c07d4ce2c15193907ba1"
+      sha256 "69769b0e433aeb557181665647fa1044dba3b14b618b5e0f04b2afe74ba92320"
     else
       odie "Unsupported Linux architecture for SynapSeq."
     end
@@ -39,7 +39,7 @@ class Synapseq < Formula
   def caveats
     <<~EOS
       For documentation and examples, visit:
-        https://github.com/ruanklein/synapseq
+        https://github.com/synapseq-foundation/synapseq
     EOS
   end
 
